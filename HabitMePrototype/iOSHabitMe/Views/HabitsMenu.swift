3fed6e0fa6b4441f8e603bcba213a4b98b647f30
Boyce Estes <estes.boyce@gmail.com>
2024-01-23 10:46:44 -0500

    Create context menu for long pressing habits

--- HabitMePrototype/iOSHabitMe/Views/HabitsMenu.swift
+++ HabitMePrototype/iOSHabitMe/Views/HabitsMenu.swift
@@ -8,7 +8,122 @@
 import SwiftUI
 
 
+struct ActionDetail: Hashable {
+    
+    let title: String
+    let role: ButtonRole?
+    let action: () -> Void
+    
+    
+    init(title: String, role: ButtonRole? = nil, action: @escaping () -> Void) {
+        self.title = title
+        self.role = role
+        self.action = action
+    }
+    
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+    
+    
+    public static func == (lhs: ActionDetail, rhs: ActionDetail) -> Bool {
+        lhs.title == rhs.title
+    }
+    
+    
+    static func ok(buttonAction: @escaping (() -> Void) = {}) -> ActionDetail {
+        ActionDetail(title: "OK", action: buttonAction)
+    }
+    
+    
+    static func cancel(buttonAction: @escaping (() -> Void) = {}) -> ActionDetail {
+        ActionDetail(title: "Cancel", role: .cancel, action: buttonAction)
+    }
+}
+
+
+struct AlertDetail {
+    
+    let title: String
+    let message: String
+    let actions: [ActionDetail]
+    
+    
+    init(title: String, message: String, actions: [ActionDetail] = [.ok()]) {
+        self.title = title
+        self.message = message
+        self.actions = actions
+    }
+    
+    
+    static func okAlert(title: String, message: String, buttonAction: @escaping () -> Void = { }) -> AlertDetail {
+        
+        AlertDetail(title: title, message: message, actions: [.ok(buttonAction: buttonAction)])
+    }
+    
+    
+    static func destructiveAlert(title: String, message: String, cancelAction: @escaping () -> Void = {}, destroyTitle: String, destroyAction: @escaping () -> Void) -> AlertDetail {
+        
+        AlertDetail(title: title, message: message, actions: [
+            .cancel(buttonAction: cancelAction),
+            ActionDetail(title: destroyTitle, role: .destructive, action: destroyAction)
+        ])
+    }
+}
+
+
+extension View {
+    
+    @ViewBuilder
+    func alert(showAlert: Binding<Bool>, alertDetail: AlertDetail?) -> some View {
+        
+        if let alertDetail {
+            alert(
+                alertDetail.title,
+                isPresented: showAlert,
+                presenting: alertDetail,
+                actions: { data in
+                    ForEach(alertDetail.actions, id: \.self) { alertAction in
+                        Button(alertAction.title, role: alertAction.role, action: alertAction.action)
+                    }
+                }, message: { data in
+                    Text(data.message)
+                }
+            )
+        } else {
+            alert("Unknown Issue", isPresented: showAlert, actions: { }, message: {
+                Text("Some problem has occurred with displaying an error alert - that's weird.")
+            })
+        }
+    }
+}
+
+
+enum HabitsMenuAlert {
+    case areYouSure(yesAction: () -> Void)
+    
+    func alertData() -> AlertDetail {
+        
+        switch self {
+        case let .areYouSure(yesAction):
+            return AlertDetail.destructiveAlert(
+                title: "Are you sure?",
+                message: "This will delete all of the habit's associated records as well 👀",
+                destroyTitle: "Destroy It All",
+                destroyAction: yesAction
+            )
+        }
+    }
+}
+
+
 struct HabitsMenu: View {
+    
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var showAlert: Bool = false
+    @State private var alertDetail: AlertDetail? = nil
     
     // TODO: load habits from db
     let habits: [DataHabit]
@@ -62,6 +177,7 @@ struct HabitsMenu: View {
         .clipShape(
             RoundedRectangle(cornerRadius: 20))
         .padding()
+        .alert(showAlert: $showAlert, alertDetail: alertDetail)
     }
     
     
@@ -78,5 +194,15 @@ struct HabitsMenu: View {
                 .clipShape(RoundedRectangle(cornerRadius: 10))
         }
         .buttonStyle(.plain)
+        .contextMenu {
+            Button("Delete Habit") {
+                
+                alertDetail = HabitsMenuAlert.areYouSure(yesAction: {
+                        print("destroy it")
+                    }
+                ).alertData()
+                showAlert = true
+            }
+        }
     }
 }
