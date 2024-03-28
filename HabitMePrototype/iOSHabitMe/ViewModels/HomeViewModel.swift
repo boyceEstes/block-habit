f26e4501539480618b27826755eda3ae3081421d
Boyce Estes <estes.boyce@gmail.com>
2024-03-28 14:18:02 -0400

    Ensure that we are always keeping the habitDataSource bound to the view, even when we recreate it because there is a new selected day
    There is some weird behavior where we are having the habitDataSource called twice - ideally we could eliminate this

--- HabitMePrototype/iOSHabitMe/ViewModels/HomeViewModel.swift
+++ HabitMePrototype/iOSHabitMe/ViewModels/HomeViewModel.swift
@@ -9,13 +9,22 @@ import Foundation
 import Combine
 
 
+// FIXME: For some reason when we go to another day, the habitDataSource loads data twice - investigate if there is room for optimization
+
+
 @Observable
 final class HomeViewModel: ActivityRecordCreatorOrNavigator {
     
     let blockHabitStore: CoreDataBlockHabitStore
-    let habitDataSource: HabitDataSource // Can get updated when selectedDate changes
+    var habitDataSource: HabitDataSource? // Can get updated when selectedDate changes
     
-    var selectedDay: Date
+    var selectedDay: Date {
+        didSet(oldValue) {
+            if oldValue != selectedDay {
+                bindHabitDataSource()
+            }
+        }
+    }
     
     var goToCreateActivityRecordWithDetails: (Habit, Date) -> Void
     var habits = [Habit]() {
@@ -28,17 +37,16 @@ final class HomeViewModel: ActivityRecordCreatorOrNavigator {
     }
     
     var cancellables = Set<AnyCancellable>()
+    var habitDataSourceCancellable: AnyCancellable?
     
     
     init(
         blockHabitStore: CoreDataBlockHabitStore,
         goToCreateActivityRecordWithDetails: @escaping (Habit, Date) -> Void
     ) {
-        
         let today = Date().noon!
         
         self.blockHabitStore = blockHabitStore
-        self.habitDataSource = blockHabitStore.habitDataSource(selectedDay: today)
         self.selectedDay = today
         self.goToCreateActivityRecordWithDetails = goToCreateActivityRecordWithDetails
         
@@ -46,20 +54,9 @@ final class HomeViewModel: ActivityRecordCreatorOrNavigator {
     }
     
     
-    private func bindSelectedDay() {
-        
-        /**
-         * When this changes, we want to update the HabitDataSource so that it gets only the habits for today
-         * This requires us to rebind the HabitDataSource so if any edits to the habits take place, we will see
-         * the updates without needing to do anything else
-         */
-//        selectedDay
-    }
-    
-    
     private func bindHabitDataSource() {
         
-        habitDataSource
+        habitDataSourceCancellable = blockHabitStore.habitDataSource(selectedDay: selectedDay)
             .habits
             .sink { error in
                 fatalError("THERES BEEN A HORRIBLE CRASH INVOLVING '\(error)' - prosecute to the highest degree of the law.")
@@ -67,7 +64,6 @@ final class HomeViewModel: ActivityRecordCreatorOrNavigator {
                 print("BOYCE: habit data source reloaded")
                 self.habits = habits
             }
-            .store(in: &cancellables)
     }
     
     
