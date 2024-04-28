567c1640d14174b3ad9868c0d79a0c7752cb0ddd
Boyce Estes <estes.boyce@gmail.com>
2024-04-27 23:31:01 -0400

    Create a framework for habit repository - attempting to get macOS to run unit tests

---
+++ HabitRepositoryFW/HabitRepositoryFW.h
@@ -0,0 +1,18 @@
+//
+//  HabitRepositoryFW.h
+//  HabitRepositoryFW
+//
+//  Created by Boyce Estes on 4/27/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HabitRepositoryFW.
+FOUNDATION_EXPORT double HabitRepositoryFWVersionNumber;
+
+//! Project version string for HabitRepositoryFW.
+FOUNDATION_EXPORT const unsigned char HabitRepositoryFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HabitRepositoryFW/PublicHeader.h>
+
+
---
+++ HabitRepositoryFW/Mortery.swift
@@ -0,0 +1,17 @@
+//
+//  Mortery.swift
+//  HabitRepositoryFW
+//
+//  Created by Boyce Estes on 4/27/24.
+//
+
+import Foundation
+
+public class Mortery {
+    
+    public init() {}
+    
+    public func attemptToProduceMorty() -> String{
+        return "Oh gee wiz"
+    }
+}
