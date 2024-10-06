2de88f8e785229dcddcc6cc32796801801bea0b9
Boyce Estes <estes.boyce@gmail.com>
2024-10-06 01:11:55 -0400

    Set streak and goal counter in the navigation bar

--- HabitMePrototype/iOSHabitMe/Views/HabitsSection/HabitsSection.swift
+++ HabitMePrototype/iOSHabitMe/Views/HabitsSection/HabitsSection.swift
@@ -25,7 +25,6 @@ struct HabitsSection: View {
     var body: some View {
         
         VStack(spacing: 0) {
-            VStack(spacing: 8) {
                 HStack {
                     
                     HStack {
@@ -57,34 +56,14 @@ struct HabitsSection: View {
                     .frame(maxWidth: scaledDayTitleWidth)
                     .dynamicTypeSize(...DynamicTypeSize.accessibility1)
                     
-                    
-                    
                     Spacer()
+                    
                     Button(action: goToCreateHabit) {
                         Image(systemName: "plus.circle")
                     }
                     .dynamicTypeSize(...DynamicTypeSize.accessibility1)
                 }
                 .homeDetailTitle()
-                
-                HStack {
-                    HStack(spacing: 4) {
-                        Text("Daily Goal")
-                        Text("\(completedNumberOfHabitsOnSelectedDay)/\(goalNumberOfHabitCompletionsOnSelectedDay) ⛳️")
-                            .foregroundStyle(.primary)
-                    }
-                    .foregroundStyle(.secondary)
-                    Spacer()
-                    HStack(spacing: 4) {
-                        Text("Streak")
-                        Text("\(currentStreak) 🔥")
-                            .foregroundStyle(.primary)
-                    }
-                    .foregroundStyle(.secondary)
-                }
-                .dynamicTypeSize(...DynamicTypeSize.accessibility1)
-                .font(.footnote)
-            }
             .padding(.horizontal)
             .padding(.vertical)
             
@@ -110,80 +89,6 @@ struct HabitsSection: View {
         .clipShape(
             RoundedRectangle(cornerRadius: 20))
         .padding()
-    }
-    
-    
-    // MARK: Helper Logic
-    
-    
-    // FIXME: Reevaluate when it is ready to consider scheduling habits for...
-    // ...specific days
-    //
-    // Not considering specific days is also a problem, because we not get an accurate
-    // goal whenever you are going back in time to a different day
-    //
-    // A good way to fix this would be to make a new property called `createdDate` on
-    // the Habit. And we could detect whether or not the habit was made on that same day
-    // and only then would we include it?
-    //
-    // Then also have the days that we want to display this habit for... This one will be
-    // more complicated
-    
-    
-    /**
-     * The Daily Goal will be based on the number of habit records that you
-     * need to create for a given day. This means that you will need to
-     * account for the completionGoal of each habit.
-     *
-     * **NOTE**: This does not include tasks with no completion goal
-     */
-    var goalNumberOfHabitCompletionsOnSelectedDay: Int {
-        
-        habitController.isCompletedHabits
-            .reduce(0) { partialResult, isCompletedHabit in
-                
-                guard let completionGoal = isCompletedHabit.habit.goalCompletionsPerDay else {
-                    
-                    // Do not include any habits without a completion goal in the count
-                    return partialResult
-                }
-                
-                guard let habitCreationDateAtNoon = isCompletedHabit.habit.creationDate.noon,
-                      habitController.selectedDay <= habitCreationDateAtNoon else {
-                    // Do not include any habits without a completion goal in the count
-                    return partialResult
-                }
-
-                
-                return partialResult + completionGoal
-            }
-    }
-    
-    
-    // FIXME: There is bit of an edge-case here if the user were to change their completion goals later it will show an inaccurate number
-    /**
-     * This number is calculated based on the number of records for the day.. Its occurring to me now that
-     * this number could change if we were to change the completionGoal number later....
-     *
-     */
-    var completedNumberOfHabitsOnSelectedDay: Int {
-        habitController.habitRecordsForSelectedDay
-            .reduce(0) { partialResult, habitRecord in
-                guard habitRecord.habit.goalCompletionsPerDay != nil else {
-                    // Do not include any habit records of habits with no completion goal in the count
-                    return partialResult
-                }
-                
-                // For each record, add 1
-                return partialResult + 1
-            }
-    }
-    
-    
-    /// Assuming today is the last day in habitsRecordsForDays
-    var currentStreak: Int {
-        
-        StatisticsCalculator.findCurrentUsageStreak(for: habitController.habitRecordsForDays)
     }
 }
 
--- HabitMePrototype/iOSHabitMe/Views/HomeView.swift
+++ HabitMePrototype/iOSHabitMe/Views/HomeView.swift
@@ -140,16 +140,25 @@ struct HomeView: View {
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
-
-                Button {
-                    goToSettings()
-                } label : {
-                    Image(systemName: "gear")
+                HStack {
+                    Text("\(completedNumberOfHabitsOnSelectedDay)/\(goalNumberOfHabitCompletionsOnSelectedDay) ⭐️")
+                        .foregroundStyle(.primary)
+                        .dynamicTypeSize(...DynamicTypeSize.accessibility1)
+                    
+                    Text("\(currentStreak) 🔥")
+                        .foregroundStyle(.primary)
+                    .dynamicTypeSize(...DynamicTypeSize.accessibility1)
                 }
             }
             
             ToolbarItemGroup(placement: .topBarTrailing) {
                 
+                Button {
+                    goToSettings()
+                } label : {
+                    Image(systemName: "gear")
+                }
+            
                 Button {
                     goToStatistics()
                 } label: {
@@ -186,6 +195,81 @@ struct HomeView: View {
             habitRecordVisualMode = visualMode
         }
     }
+    
+    
+    // MARK: Helper Logic
+    
+    
+    // FIXME: Reevaluate when it is ready to consider scheduling habits for...
+    // ...specific days
+    //
+    // Not considering specific days is also a problem, because we not get an accurate
+    // goal whenever you are going back in time to a different day
+    //
+    // A good way to fix this would be to make a new property called `createdDate` on
+    // the Habit. And we could detect whether or not the habit was made on that same day
+    // and only then would we include it?
+    //
+    // Then also have the days that we want to display this habit for... This one will be
+    // more complicated
+    
+    
+    /**
+     * The Daily Goal will be based on the number of habit records that you
+     * need to create for a given day. This means that you will need to
+     * account for the completionGoal of each habit.
+     *
+     * **NOTE**: This does not include tasks with no completion goal
+     */
+    var goalNumberOfHabitCompletionsOnSelectedDay: Int {
+        
+        habitController.isCompletedHabits
+            .reduce(0) { partialResult, isCompletedHabit in
+                
+                guard let completionGoal = isCompletedHabit.habit.goalCompletionsPerDay else {
+                    
+                    // Do not include any habits without a completion goal in the count
+                    return partialResult
+                }
+                
+                guard let habitCreationDateAtNoon = isCompletedHabit.habit.creationDate.noon,
+                      habitController.selectedDay <= habitCreationDateAtNoon else {
+                    // Do not include any habits without a completion goal in the count
+                    return partialResult
+                }
+
+                
+                return partialResult + completionGoal
+            }
+    }
+    
+    
+    // FIXME: There is bit of an edge-case here if the user were to change their completion goals later it will show an inaccurate number
+    /**
+     * This number is calculated based on the number of records for the day.. Its occurring to me now that
+     * this number could change if we were to change the completionGoal number later....
+     *
+     */
+    var completedNumberOfHabitsOnSelectedDay: Int {
+        habitController.habitRecordsForSelectedDay
+            .reduce(0) { partialResult, habitRecord in
+                guard habitRecord.habit.goalCompletionsPerDay != nil else {
+                    // Do not include any habit records of habits with no completion goal in the count
+                    return partialResult
+                }
+                
+                // For each record, add 1
+                return partialResult + 1
+            }
+    }
+    
+    
+    /// Assuming today is the last day in habitsRecordsForDays
+    var currentStreak: Int {
+        
+        StatisticsCalculator.findCurrentUsageStreak(for: habitController.habitRecordsForDays)
+    }
+
 }
 
 
@@ -201,6 +285,12 @@ struct HomeView: View {
             goToStatistics: { },
             goToCreateActivityRecordWithDetails: { _, _ in },
             goToSettings: { }
+        )
+        .environmentObject(
+            HabitController(
+                blockHabitRepository: CoreDataBlockHabitStore.preview(),
+                selectedDay: Date()
+            )
         )
     }
 }
